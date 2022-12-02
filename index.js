55f98cfbb72063e568a375588353d860fe346beb
Ian <muneneian5@gmail.com>
2022-12-02 07:59:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solution in this file!
+// Code your solution in this file
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Lake City';
+let companyCeo = 'Bryan Chucho'; 
